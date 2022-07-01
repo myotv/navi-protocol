5cad707378a94a9d42c07c122fbc7e94440fb2ee
Pavel Nikiforov <pavel@ip-soft.ru>
2022-07-01 12:48:49 +0300

    add navi-config.h

---
+++ navi-config.h
@@ -0,0 +1,13 @@
+#ifndef _NAVI_CONFIG_H_
+#define _NAVI_CONFIG_H_
+
+#define WITH_DEBUG 1
+#define DEBUG_DATA_PACKETS 1
+
+// allow to create __attribute__((constructor)) initializer, 
+// otherwize must call navi_init_library() method
+#ifndef NAVI_ALLOW_CONSTRUCTOR_INIT
+#define NAVI_ALLOW_CONSTRUCTOR_INIT 1
+#endif
+
+#endif
